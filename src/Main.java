7078d97c94913ad3e4e929621b42a794c947757e
milcarzg <milcarzg@gmail.com>
2015-02-16 00:52:33 +0100

    code cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import java.awt.Point;
-import java.util.Arrays;
+
 
 public class Main 
 {
-	private static String _room;
 	private static String[] size;
-	private static String _origin;
 	private static String[] _coordinates;
 	private static char[] _steps;
-	private static String _moves;
 	
 	public static void main(String[] args)
 	{
